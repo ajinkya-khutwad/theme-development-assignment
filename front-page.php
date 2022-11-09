3d39262788ebb5a208d8f6756293c649d6d84ed0
ajinkya-khutwad <ajinkya.khutwad@hbwsl.com>
2022-11-09 22:30:55 +0530

    Design HomePage

--- front-page.php
+++ front-page.php
@@ -6,6 +6,36 @@
  */
 
 get_header();
+?>
 
-get_footer();
+<section class="banner" style="background-image: url('<?php echo esc_url( get_the_post_thumbnail_url() ); ?>');">
+	<div class="banner-content">
+		<span>
+			<h1 class="banner-title">Gearing up the ideas</h1>
+			<p class="banner-para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus alias, illo tempora recusandae consequatur molestias quis cumque iure, repudiandae earum, asperiores perspiciatis officia mollitia.</p>
+		</span>
+	</div>
+</section>
 
+<?php
+	get_template_part( 'template-parts/content', 'features' );
+?>
+
+<!-- Gallery section -->
+<section class="gallery">
+	<div class="container">
+		<div class="gallery-title-area">
+			<h2>D'SIGN IN THE SOUL</h2>
+			<button onclick="designfly_goto_portfolio()" class="gallery-view-all">View all</button>
+		</div>
+		<hr>
+		<?php get_template_part( 'template-parts/content', 'portfolio' ); ?>
+		<hr>
+	</div>
+</section>
+
+<script type="text/javascript">
+	const designfly_goto_portfolio = () => window.location.href = '<?php echo esc_url( get_site_url() ); ?>/portfolio';
+</script>
+
+<?php get_footer(); ?>
